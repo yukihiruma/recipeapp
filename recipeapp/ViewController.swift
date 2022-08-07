7e23eaac8c1d9da724554ab15980c2893c712a19
yuki.hiruma <yminetajp@gmail.com>
2022-08-07 10:14:25 +0900

    Initial Commit

---
+++ recipeapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recipeapp
+//
+//  Created by yuki hiruma on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
